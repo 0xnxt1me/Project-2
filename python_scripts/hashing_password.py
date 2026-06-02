ff89b61cb6672d85224f2706b40acda29692bee7
Fabio <2123422@student.uma.pt>
2026-06-02 16:43:32 +0100

    feat: 4.1 assignment 2

--- python_scripts/hashing_password.py
+++ python_scripts/hashing_password.py
@@ -3,14 +3,18 @@ import sys
 
 
 def main():
-    verify_arguments(sys.argv)
-    salt = sys.argv[1]
-    password = sys.argv[2]
+    if len(sys.argv) < 4:
+        print("Usage: python3 hashing_password.py <username> <hex_salt> <password>")
+        sys.exit(1)
+    user = sys.argv[1]
+    salt = sys.argv[2]
+    password = sys.argv[3]
     
+    user_bytes = user.encode('utf-8')
     salt_bytes = bytes.fromhex(salt)
     password_bytes = password.encode('utf-8')
     
-    combined_bytes = salt_bytes + password_bytes
+    combined_bytes = user_bytes + salt_bytes + password_bytes
     
     result_hash = hashlib.md5(combined_bytes).hexdigest()
     
